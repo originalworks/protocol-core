c0ef0f7b1435a0838a07c77044f5ba36d8957e2c
Daniel Cybulka <danielcybulka@gmail.com>
2024-11-29 10:11:05 +0100

    fix env var passing in contracts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -18,7 +18,7 @@ const config: HardhatUserConfig = {
       ],
     },
     kurtosis_testnet: {
-      url: process.env.RPC_URL,
+      url: `${process.env.RPC_URL}`,
       accounts: [
         "0xbcdf20249abf0ed6d944c0288fad489e33f66b3960d9e6229c1cd214ed3bbe31",
       ],
