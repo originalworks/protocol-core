043f441e4bdc4b77a7fc99ba8f3a1959e62cb5b1
Daniel Cybulka <danielcybulka@gmail.com>
2025-02-05 12:29:47 +0100

    fix sequencer address

--- owen/src/constants.rs
+++ owen/src/constants.rs
@@ -1,6 +1,6 @@
 use alloy::primitives::{address, Address};
 
-pub const DDEX_SEQUENCER_ADDRESS: Address = address!("00c042C4D5D913277CE16611a2ce6e9003554aD5");
+pub const DDEX_SEQUENCER_ADDRESS: Address = address!("EAdd25177cd288aE9f90596938f934Bd20b3531a");
 pub const IPFS_API_BASE_URL: &str = "http://localhost:5001";
 pub const IPFS_API_ADD_FILE: &str = "/api/v0/add";
 pub const IPFS_API_CAT_FILE: &str = "/api/v0/cat";
