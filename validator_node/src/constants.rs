894478e456193e12d6cc08f0e3e86dfbc499338c
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-02-21 12:38:12 +0100

    Deployment of contracts to DEV and STAGE

--- validator_node/src/constants.rs
+++ validator_node/src/constants.rs
@@ -1,6 +1,6 @@
 use alloy::primitives::FixedBytes;
 
-pub const DDEX_SEQUENCER_ADDRESS: &str = "09A5a916b7a37C035E268A9EefCD182cC9cA51E3";
+pub const DDEX_SEQUENCER_ADDRESS: &str = "dF83A1529C73CA77b1fc6323C103B9F84b57f687";
 pub const GET_BEACON_BLOCK_API_PATH: &str = "/eth/v2/beacon/blocks/";
 pub const GET_SIDECARS_API_PATH: &str = "/eth/v1/beacon/blob_sidecars/";
 pub const EMPTY_QUEUE_HEAD: FixedBytes<32> = FixedBytes::repeat_byte(0);
