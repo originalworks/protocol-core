6225568df2255f427e4674623701ba8b86e761f4
Bartek Strużyk <bartlomiejstruzyk@gmail.com>
2025-02-27 09:32:31 +0100

    updated mapping to fix indexing issue of DdexEmitter BlobProcessed event

--- registry-subgraph/src/mapping.ts
+++ registry-subgraph/src/mapping.ts
@@ -1,16 +1,31 @@
-import { BlobProcessed } from "./types/DdexEmitter/DdexEmitter";
+import { log } from "@graphprotocol/graph-ts";
+
 import { ProvedMessage } from "./types/schema";
+import { BlobProcessed } from "./types/DdexEmitter/DdexEmitter";
 
 export function handleBlobProcessed(event: BlobProcessed): void {
-  for (let i = 0; i < event.params.proverPublicOutputs.messages.length; i++) {
-    const message = event.params.proverPublicOutputs.messages[i];
+  const proverPublicOutputs = event.params.proverPublicOutputs;
+  const messages = proverPublicOutputs.messages;
+
+  const isValid = proverPublicOutputs.is_valid;
+
+  log.info("isValid: {}", [isValid.toString()])
+
+  for (let i = 0; i < messages.length; i++) {
+    const message = messages[i];
 
     const provedMessage = new ProvedMessage(
       `${event.transaction.hash.toHex()}-${i}`
     );
-    provedMessage.message_id = message.release.release_id.toString();
+    provedMessage.message_id = message.release.release_id.icpn.toString();
     provedMessage.timestamp = event.block.timestamp;
     provedMessage.validator = event.transaction.from;
     provedMessage.save();
+
+    log.info("Processed message subtitle: {}, icpn: {}, title_text: {}", [
+      message.release.subtitle.toString(),
+      message.release.release_id.icpn.toString(),
+      message.release.title_text.toString(),
+    ])
   }
 }
