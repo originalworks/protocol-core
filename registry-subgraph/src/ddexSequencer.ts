e44a6150af90a5787307103dbd23785ef3d2b9a0
Bartek-str <bartlomiejstruzyk@gmail.com>
2025-03-03 08:45:05 +0100

    cleanup

--- registry-subgraph/src/ddexSequencer.ts
+++ registry-subgraph/src/ddexSequencer.ts
@@ -1,30 +1,11 @@
-import { log, BigInt, ethereum } from '@graphprotocol/graph-ts';
+import { BigInt } from "@graphprotocol/graph-ts";
 
-import { BlobCall, Blobs } from './types/schema';
-import { NewBlobSubmitted, SubmitNewBlobCall } from './types/DdexSequencer/DdexSequencer';
-import { json } from '@graphprotocol/graph-ts/common/json';
+import { Blobs } from "./types/schema";
+import { NewBlobSubmitted } from "./types/DdexSequencer/DdexSequencer";
 
-const BlobsSubmittedCallId = "submitNewBlob";
 const BlobsSubmittedEventId = "NewBlobSubmitted";
 
-export function handleSubmitNewBlob(call: SubmitNewBlobCall): void {
-  log.info("Received submitNewBlob call", []);
-
-  let blobs = Blobs.load(BlobsSubmittedCallId);
-
-  if (blobs == null) {
-    blobs = new Blobs(BlobsSubmittedCallId);
-    blobs.amount = BigInt.zero();
-  }
-
-  blobs.amount = blobs.amount.plus(BigInt.fromI32(1));
-
-  blobs.save();
-}
-
 export function handleNewBlobSubmitted(event: NewBlobSubmitted): void {
-  log.info("Received NewBlobSubmitted event", []);
-
   let blobs = Blobs.load(BlobsSubmittedEventId);
 
   if (blobs == null) {
