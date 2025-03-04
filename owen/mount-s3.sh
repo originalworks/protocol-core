cd508da05bd8c8751bf8d0d20fe2916d7a60e5f3
Alejandro <alejandro@criadoperez.com>
2025-03-04 12:39:50 +0100

    Fix #116: Mount S3 bucket on owen my_messages folder

--- owen/mount-s3.sh
+++ owen/mount-s3.sh
@@ -11,7 +11,7 @@ echo "${AWS_ACCESS_KEY_ID}:${AWS_SECRET_ACCESS_KEY}" > /etc/passwd-s3fs
 chmod 600 /etc/passwd-s3fs
 
 # Mount the S3 bucket
-s3fs "${S3_BUCKET_NAME}" /protocol-core/owen/input \
+s3fs "${S3_BUCKET_NAME}" /protocol-core/owen/my_messages \
   -o passwd_file=/etc/passwd-s3fs \
   -o url="https://s3.${AWS_DEFAULT_REGION}.amazonaws.com" \
   -o use_path_request_style
