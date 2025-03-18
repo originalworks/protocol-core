04d68fc22883a44fca6b3fa646bec18f97f7a3c5
Bartek Strużyk <bartlomiejstruzyk@gmail.com>
2025-03-18 14:10:48 +0100

    added safe json parsing and error handling for blob metadata

--- registry-subgraph/src/blobMetadata.ts
+++ registry-subgraph/src/blobMetadata.ts
@@ -1,8 +1,10 @@
 import {
+  log,
   json,
   Bytes,
   dataSource
-} from "@graphprotocol/graph-ts";
+} from '@graphprotocol/graph-ts';
+import { JSONValueKind } from "@graphprotocol/graph-ts/common/value";
 
 import { getValueIfExist } from "./helpers";
 import { BlobMetadata } from "./types/schema";
@@ -10,16 +12,22 @@ import { BlobMetadata } from "./types/schema";
 export function handleBlobMetadata(content: Bytes): void {
   const blobMetadata = new BlobMetadata(dataSource.stringParam());
 
-  const metadata = json.fromBytes(content).toObject();
+  const jsonResult = json.try_fromBytes(content);
 
-  if (metadata) {
-    blobMetadata.versioned_hash = getValueIfExist(metadata, "versioned_hash");
-    blobMetadata.transaction_hash = getValueIfExist(metadata, "transaction_hash");
-    blobMetadata.block_number = getValueIfExist(metadata, "block_number");
-    blobMetadata.timestamp = getValueIfExist(metadata, "timestamp");
-    blobMetadata.chain_id = getValueIfExist(metadata, "chain_id");
-    blobMetadata.network_name = getValueIfExist(metadata, "network_name");
-    blobMetadata.blob_ipfs_cid = getValueIfExist(metadata, "blob_ipfs_cid");
+  if (jsonResult.isOk) {
+    if (jsonResult.value.kind == JSONValueKind.OBJECT) {
+      const metadata = jsonResult.value.toObject()
+
+      blobMetadata.versioned_hash = getValueIfExist(metadata, "versioned_hash");
+      blobMetadata.transaction_hash = getValueIfExist(metadata, "transaction_hash");
+      blobMetadata.block_number = getValueIfExist(metadata, "block_number");
+      blobMetadata.timestamp = getValueIfExist(metadata, "timestamp");
+      blobMetadata.chain_id = getValueIfExist(metadata, "chain_id");
+      blobMetadata.network_name = getValueIfExist(metadata, "network_name");
+      blobMetadata.blob_ipfs_cid = getValueIfExist(metadata, "blob_ipfs_cid");
+    }
+  } else {
+    log.warning("Failed to parse JSON from Bytes: {}, CID: {}", [content.toHexString(), dataSource.stringParam()]);
   }
 
   blobMetadata.save();
