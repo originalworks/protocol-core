a6873a9d0d97f9021ca103a92691271a6c744737
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-04-24 14:24:04 +0200

    Install Docker, nvidia container toolkit and nvidia driver

---
+++ aws/validator-infra/instance-config/install-nvidia-driver.sh
@@ -0,0 +1,3 @@
+wget https://us.download.nvidia.com/tesla/570.124.06/nvidia-driver-local-repo-ubuntu2404-570.124.06_1.0-1_amd64.deb
+cp /var/nvidia-driver-local-repo-ubuntu2404-570.124.06/nvidia-driver-local-D67F55A1-keyring.gpg /usr/share/keyrings/
+dpkg -i nvidia-driver-local-repo-ubuntu2404-570.124.06_1.0-1_amd64.deb
