9c8439330f9dd80c08d575526315d9c3a392d358
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-04-28 12:43:10 +0200

    install awscli

--- aws/validator-infra/rebuild.sh
+++ aws/validator-infra/rebuild.sh
@@ -1,5 +1,8 @@
 # WIP - dummy file
 echo "there was a rebuild attempt!" > /workspace/rebuild-attempt.txt
-git pull
 docker stop $(docker ps -q)
+git pull
+rm ./validator_node/.env
+cp ./aws/validator_infra/.env ./validator_node/.env
+
 docker build --no-cache -t validator-node-image ./validator_node/ 
