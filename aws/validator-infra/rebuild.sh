ca7e7b2cc184be75a2594accd1dfba4f31c544fc
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-04-28 11:32:20 +0200

    Validator CICD - rebuild script

--- aws/validator-infra/rebuild.sh
+++ aws/validator-infra/rebuild.sh
@@ -1,2 +1,5 @@
 # WIP - dummy file
 echo "there was a rebuild attempt!" > /workspace/rebuild-attempt.txt
+git pull
+docker stop $(docker ps -q)
+docker build --no-cache -t validator-node-image ./validator_node/ 
