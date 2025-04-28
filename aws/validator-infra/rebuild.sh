fe705345c715193a0f3e3973d9791cdc5e3fc1a8
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-04-28 12:43:57 +0200

    Do not build docker image yet

--- aws/validator-infra/rebuild.sh
+++ aws/validator-infra/rebuild.sh
@@ -5,4 +5,4 @@ git pull
 rm ./validator_node/.env
 cp ./aws/validator_infra/.env ./validator_node/.env
 
-docker build --no-cache -t validator-node-image ./validator_node/ 
+# docker build --no-cache -t validator-node-image ./validator_node/ 
