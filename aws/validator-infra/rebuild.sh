17941821a6ea5aadad4429ef5b2838b783f947ca
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-04-30 13:04:04 +0200

    Add rebuild and run script

--- aws/validator-infra/rebuild.sh
+++ aws/validator-infra/rebuild.sh
@@ -1,15 +1,16 @@
-echo "there was a rebuild attempt!" > /workspace/rebuild-attempt.txt
+echo "Starting docker image rebuild..."
 docker stop $(docker ps -q)
+cd /workspace/protocol-core
 git pull
 
 # recreate .env file
 secret_name=$(jq -r '.Parameters.ValidatorSecretsName' /workspace/protocol-core/aws/validator-infra/template-config.json)
-rm -f ./validator_node/.env
-cp ./aws/validator-infra/.env ./validator_node/.env
-echo >> ./validator_node/.env
+rm -f /workspace/.env
+cp /workspace/protocol-core/aws/validator-infra/validator.env /workspace/protocol-core/validator_node/.env
+echo >> /workspace/protocol-core/validator_node/.env
 aws secretsmanager get-secret-value \
     --secret-id $secret_name \
     --query SecretString \
-    --output text | jq -r 'to_entries[] | "\(.key)=\(.value)"' >> ./validator_node/.env
+    --output text | jq -r 'to_entries[] | "\(.key)=\(.value)"' >> /workspace/protocol-core/validator_node/.env
 
-# docker build --no-cache -t validator-node-image ./validator_node/ 
+docker build --no-cache -t validator-node-image /workspace/protocol-core/validator_node/ 
