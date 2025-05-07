928d7bf8a854134e6ae10207a71bd972b662b522
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-05-07 16:03:26 +0200

    Do not autoupdate nvidia driver on ec2

--- aws/validator-infra/instance-config/install-nvidia-driver.sh
+++ aws/validator-infra/instance-config/install-nvidia-driver.sh
@@ -4,3 +4,4 @@ dpkg -i /workspace/nvidia-driver-local-repo-ubuntu2404-570.124.06_1.0-1_amd64.de
 cp /var/nvidia-driver-local-repo-ubuntu2404-570.124.06/nvidia-driver-local-D67F55A1-keyring.gpg /usr/share/keyrings/
 apt update
 apt install nvidia-driver-570=570.124.06-0ubuntu1 -y
+apt-mark hold nvidia-driver-570
