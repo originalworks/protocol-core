044545a800d584524d38de5fe22e97756187589b
Daniel Cybulka <danielcybulka@gmail.com>
2025-05-07 15:52:12 +0200

    add storacha to owen

--- owen/src/constants.rs
+++ owen/src/constants.rs
@@ -2,7 +2,9 @@ pub const DDEX_SEQUENCER_ADDRESS: &str = "75AbeCf07C26368F0f4AA0b0d3637A732E2546
 pub const IPFS_API_BASE_URL: &str = "http://localhost:5001";
 pub const IPFS_API_ADD_FILE: &str = "/api/v0/add";
 pub const IPFS_API_CAT_FILE: &str = "/api/v0/cat";
-
+pub const DEFAULT_STORACHA_BRIDGE_URL: &str =
+    "https://p1o0h55rwh.execute-api.us-east-1.amazonaws.com/devel/";
+pub const CLIENT: &[u8] = b"OWEN";
 #[cfg(any(feature = "local-s3"))]
 pub const DEFAULT_DATABASE_NAME: &str = "sqlite";
 
