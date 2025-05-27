5377cae41cdcc6f37292274111939bcc51b83b46
cezary-stroczynski <cezary.stroczynski@gmail.com>
2025-05-27 12:30:25 +0200

    Local setup with dev mode proving

---
+++ local_setup/dev_mode_network/scripts/validator/turn_off.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DOCKER_FOLDER=$SCRIPT_DIR/../../docker
+
+# This script will turn off one of three validators
+# VALIDATOR variable define which validator to turn off:
+#
+#   VALIDATOR=1 ./turn_off.sh
+#
+# this will turn off the validator 1 in its container
+# Available validators: 1, 2, 3
+
+# If you omit VALIDATOR variable, it will try to turn off validator 1 by default
+
+VALIDATOR_CONTAINER_NAME="validator_node_${VALIDATOR:-1}"
+
+HOST_UID=$(id -u) HOST_GID=$(id -g) docker compose -f $DOCKER_FOLDER/docker-compose.yml exec $VALIDATOR_CONTAINER_NAME sh -c "pkill -f validator_node"
