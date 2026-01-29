82f45ecafef916cc7161012114854f537633e836
Bartek Strużyk <bartlomiejstruzyk@gmail.com>
2026-01-29 11:40:44 +0100

    cleanup

--- data-provider-isrc/src/mapping.ts
+++ data-provider-isrc/src/mapping.ts
@@ -1,12 +1,7 @@
-// import { BigInt } from "@graphprotocol/graph-ts";
 import { NewBlob, Submitter, TrackProcessedWithSubmitter } from "./types/schema";
 
-import {
-  DdexSequencer,
-  NewBlobSubmitted,
-  SubmitNewBlobCall,
-} from "./types/DdexSequencer/DdexSequencer";
 import { BlobProcessed } from "./types/DdexEmitter/DdexEmitter";
+import { SubmitNewBlobCall } from "./types/DdexSequencer/DdexSequencer";
 
 export function handleNewBlobSubmitted(call: SubmitNewBlobCall): void {
   const id = call.inputs._blobSha2.toHex().toString();
@@ -37,23 +32,6 @@ export function handleNewBlobSubmitted(call: SubmitNewBlobCall): void {
 
   newBlob.save();
 }
-
-// export function handleNewBlobSubmitted(event: NewBlobSubmitted): void {
-//   const id = event.params.commitment.toHex().toString();
-
-//   let newBlob = NewBlob.load(id);
-
-//   if (newBlob == null) {
-//     newBlob = new NewBlob(id);
-//     newBlob.msgSender = event.transaction.from;
-//     newBlob.commitment = event.params.commitment;
-//     newBlob.save();
-//   } else {
-//     newBlob.msgSender = event.transaction.from;
-//     newBlob.commitment = event.params.commitment;
-//     newBlob.save();
-//   }
-// }
 
 export function handleBlobProcessed(event: BlobProcessed): void {
   const blobSha2 = event.params.proverPublicOutputs.digest;
@@ -95,37 +73,4 @@ export function handleBlobProcessed(event: BlobProcessed): void {
       }
     }
   }
-
-  // if (submittedBlob) {
-  //   const id = event.transaction.hash.toHex().toString();
-  //   const blobProcessedWithSubmitter = new BlobProcessedWithSubmitter(id);
-  //   blobProcessedWithSubmitter.firstProvedMessageReleaseText =
-  //     event.params.proverPublicOutputs.messages[0].release.title_text;
-  //   blobProcessedWithSubmitter.submitter = submittedBlob.blobSubmitter;
-  //   blobProcessedWithSubmitter.save();
-  // } else {
-  //   const id = event.transaction.hash.toHex().toString();
-  //   const blobProcessedWithSubmitter = new BlobProcessedWithSubmitter(id);
-  //   blobProcessedWithSubmitter.firstProvedMessageReleaseText =
-  //     "here submitter is a blobsha2";
-  //   blobProcessedWithSubmitter.submitter = blobSha2;
-  //   blobProcessedWithSubmitter.save();
-  // }
-  //
-  //   blobProcessedWithSubmitter.submitter =
-  // if (provedMessageSubmitter == null) {
-  // provedMessageSubmitter.blobDigest = event.params.proverPublicOutputs.digest;
-  // provedMessageSubmitter.submitter = event.params.proverPublicOutputs.digest;
-
-  // let sequencer = DdexSequencer.bind(
-  //   Address.fromString("0x75AbeCf07C26368F0f4AA0b0d3637A732E25467e")
-  // );
-
-  // let value = sequencer.try_getQueueHeadDetails();
-  // if (value) {
-  //   provedMessageSubmitter.submitter = value.value.value0.proposer;
-  // }
-
-  // provedMessageSubmitter.save();
-  //   }
 }
