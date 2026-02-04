559577777c6ad42a49f84fcbad3acf91fe16e74f
Bartek Strużyk <bartlomiejstruzyk@gmail.com>
2026-02-04 16:18:39 +0100

    simplified registry subgraph

--- registry-subgraph/src/ddexEmitter.ts
+++ registry-subgraph/src/ddexEmitter.ts
@@ -1,31 +1,18 @@
 import { BigInt, log } from "@graphprotocol/graph-ts";
 
 import {
-  BlobRejectedEventId,
-  recordBlobsStatuses,
-  BlobProcessedEventId,
-  deduplicateStringList,
-  recordHealthStatusValidatorData,
-} from "./helpers";
-import {
   Cid,
   Track,
   Artist,
   Release,
   ProvedMessage,
   TracksAddedPerDay,
-  ValidatorTxPerDay,
   ArtistsAddedPerDay,
-  BlobsRejectedPerDay,
   TracksAddedPerMonth,
-  ValidatorTxPerMonth,
   ArtistsAddedPerMonth,
-  BlobsProcessedPerDay,
-  BlobsRejectedPerMonth,
-  BlobsProcessedPerMonth,
-  MessagesProcessedPerDay,
 } from "./types/schema";
-import { BlobProcessed, BlobRejected } from "./types/DdexEmitter/DdexEmitter";
+import { deduplicateStringList } from "./helpers";
+import { BlobProcessed } from "./types/DdexEmitter/DdexEmitter";
 import { AssetMetadataTemplate, BlobMetadataTemplate } from "./types/templates";
 
 export function handleBlobProcessed(event: BlobProcessed): void {
@@ -81,20 +68,6 @@ export function handleBlobProcessed(event: BlobProcessed): void {
     release.imageMetadata = cid.id;
     release.timestamp = event.block.timestamp;
     release.save();
-
-    let messagesProcessed = MessagesProcessedPerDay.load(id);
-
-    if (messagesProcessed == null) {
-      messagesProcessed = new MessagesProcessedPerDay(id);
-      messagesProcessed.amount = BigInt.zero();
-    }
-
-    messagesProcessed.month = (date.getUTCMonth() + 1).toString();
-    messagesProcessed.day = date.getUTCDate().toString();
-    messagesProcessed.year = date.getUTCFullYear().toString();
-    messagesProcessed.amount = messagesProcessed.amount.plus(BigInt.fromI32(1));
-
-    messagesProcessed.save();
 
     const displayArtistNames = mRelease.display_artist_names;
     if (displayArtistNames.length > 0) {
@@ -236,107 +209,8 @@ export function handleBlobProcessed(event: BlobProcessed): void {
     }
   }
 
-  recordBlobsStatuses(BlobProcessedEventId, event.block.timestamp, event.transaction.hash);
-  recordHealthStatusValidatorData(event.block.timestamp, event.transaction.hash);
-  handleValidatorData(event.block.timestamp);
-
-  let blobsProcessed = BlobsProcessedPerDay.load(id);
-
-  if (blobsProcessed == null) {
-    blobsProcessed = new BlobsProcessedPerDay(id);
-    blobsProcessed.amount = BigInt.zero();
-  }
-
-  blobsProcessed.month = (date.getUTCMonth() + 1).toString();
-  blobsProcessed.day = date.getUTCDate().toString();
-  blobsProcessed.year = date.getUTCFullYear().toString();
-  blobsProcessed.amount = blobsProcessed.amount.plus(BigInt.fromI32(1));
-
-  blobsProcessed.save();
-
-  let processedPerMonth = BlobsProcessedPerMonth.load(idPerMonth);
-
-  if (processedPerMonth == null) {
-    processedPerMonth = new BlobsProcessedPerMonth(idPerMonth);
-    processedPerMonth.amount = BigInt.zero();
-  }
-
-  processedPerMonth.month = (date.getUTCMonth() + 1).toString();
-  processedPerMonth.year = date.getUTCFullYear().toString();
-  processedPerMonth.amount = processedPerMonth.amount.plus(BigInt.fromI32(1));
-
-  processedPerMonth.save();
-
   log.info("BlobProcessed CID: {}", [event.params.cid]);
 
   const blobMetadataIPFSPath = event.params.cid + "/blob/metadata.json";
   BlobMetadataTemplate.create(blobMetadataIPFSPath);
-}
-
-export function handleBlobRejected(event: BlobRejected): void {
-  recordBlobsStatuses(BlobRejectedEventId, event.block.timestamp, event.transaction.hash);
-  recordHealthStatusValidatorData(event.block.timestamp, event.transaction.hash);
-  handleValidatorData(event.block.timestamp);
-
-  const date = new Date(BigInt.fromString(`${event.block.timestamp.toI64()}000`).toI64());
-  const id = `${date.getUTCMonth() + 1}-${(date.getUTCDate())}-${date.getUTCFullYear()}`;
-  let blobsRejected = BlobsRejectedPerDay.load(id);
-
-  if (blobsRejected == null) {
-    blobsRejected = new BlobsRejectedPerDay(id);
-    blobsRejected.amount = BigInt.zero();
-  }
-
-  blobsRejected.month = (date.getUTCMonth() + 1).toString();
-  blobsRejected.day = date.getUTCDate().toString();
-  blobsRejected.year = date.getUTCFullYear().toString();
-  blobsRejected.amount = blobsRejected.amount.plus(BigInt.fromI32(1));
-
-  blobsRejected.save();
-
-  const idPerMonth = `${date.getUTCMonth() + 1}-1-${date.getUTCFullYear()}`;
-  let rejectedPerMonth = BlobsRejectedPerMonth.load(idPerMonth);
-
-  if (rejectedPerMonth == null) {
-    rejectedPerMonth = new BlobsRejectedPerMonth(idPerMonth);
-    rejectedPerMonth.amount = BigInt.zero();
-  }
-
-  rejectedPerMonth.month = (date.getUTCMonth() + 1).toString();
-  rejectedPerMonth.year = date.getUTCFullYear().toString();
-  rejectedPerMonth.amount = rejectedPerMonth.amount.plus(BigInt.fromI32(1));
-
-  rejectedPerMonth.save();
-}
-
-function handleValidatorData(validationTimestamp: BigInt): void {
-  const date = new Date(BigInt.fromString(`${validationTimestamp.toI64()}000`).toI64());
-  const id = `${date.getUTCMonth() + 1}-${(date.getUTCDate())}-${date.getUTCFullYear()}`;
-  let validatorDay = ValidatorTxPerDay.load(id);
-
-  if (validatorDay == null) {
-    validatorDay = new ValidatorTxPerDay(id);
-    validatorDay.amount = BigInt.zero();
-  }
-
-  validatorDay.month = (date.getUTCMonth() + 1).toString();
-  validatorDay.day = date.getUTCDate().toString();
-  validatorDay.year = date.getUTCFullYear().toString();
-  validatorDay.amount = validatorDay.amount.plus(BigInt.fromI32(1));
-
-  validatorDay.save();
-
-  const idMonth = `${date.getUTCMonth() + 1}-1-${date.getUTCFullYear()}`;
-  let validatorMonth = ValidatorTxPerMonth.load(idMonth);
-
-  if (validatorMonth == null) {
-    validatorMonth = new ValidatorTxPerMonth(idMonth);
-    validatorMonth.amount = BigInt.zero();
-  }
-
-  validatorMonth.month = (date.getUTCMonth() + 1).toString();
-  validatorMonth.year = date.getUTCFullYear().toString();
-  validatorMonth.amount = validatorMonth.amount.plus(BigInt.fromI32(1));
-
-  validatorMonth.save();
 }
