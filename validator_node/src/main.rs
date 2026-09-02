b7ddcd60ac1d5fdf4077db2c71752a88b76ce2df
Daniel Cybulka <danielcybulka@gmail.com>
2026-09-02 16:16:43 +0200

    add sentry log integration

--- validator_node/src/main.rs
+++ validator_node/src/main.rs
@@ -11,17 +11,15 @@ mod heartbeat;
 
 fn init_sentry(config: &Config) -> Option<sentry::ClientInitGuard> {
     if !config.disable_telemetry {
-        let guard: sentry::ClientInitGuard = sentry::init((
-            "https://8a5e3e61ac0beb391ad84b32086674df@o4508766269014016.ingest.us.sentry.io/4508805625217024",
-            sentry::ClientOptions {
-                environment: Some(config.environment.to_owned().into()),
-                release: sentry::release_name!(),
-                attach_stacktrace: true,
-                auto_session_tracking: true,
-                traces_sample_rate: 1.0, // lower it in production
-                ..Default::default()
-            },
-        ));
+        let options = sentry::ClientOptions::new()
+            .dsn("https://8a5e3e61ac0beb391ad84b32086674df@o4508766269014016.ingest.us.sentry.io/4508805625217024")
+            .environment(config.environment.to_owned())
+            .maybe_release(sentry::release_name!())
+            .attach_stacktrace(true)
+            .auto_session_tracking(true)
+            .traces_sample_rate(1.0); // lower it in production
+
+        let guard = sentry::init(options);
         log_info!("Telemetry has been initiated");
         Some(guard)
     } else {
@@ -34,7 +32,7 @@ fn init_logging() -> anyhow::Result<()> {
     let mut log_builder = pretty_env_logger::formatted_timed_builder();
     log_builder.parse_filters("info");
 
-    let logger = sentry_log::SentryLogger::with_dest(log_builder.build());
+    let logger = sentry::integrations::log::SentryLogger::with_dest(log_builder.build());
     log::set_boxed_logger(Box::new(logger)).with_context(|| "Failed to set boxed logger")?;
     log::set_max_level(log::LevelFilter::Info);
 
@@ -72,12 +70,12 @@ async fn init(config: Config) -> anyhow::Result<()> {
 }
 
 fn main() -> anyhow::Result<()> {
-    init_logging()?;
-
     dotenvy::dotenv().ok();
 
     let config = Config::build();
     let _guard = init_sentry(&config);
+
+    init_logging()?;
 
     tokio::runtime::Builder::new_multi_thread()
         .enable_all()
