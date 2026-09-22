55ca06aeee50c259e4fc66f0ec3b22982ce7218a
Daniel Cybulka <danielcybulka@gmail.com>
2026-09-22 14:11:01 +0200

    remove unused import

--- contracts/scripts/execute/deployRiscZeroVerifierRouter.ts
+++ contracts/scripts/execute/deployRiscZeroVerifierRouter.ts
@@ -1,10 +1,9 @@
-import { ethers } from "hardhat";
 import { deployRiscZeroVerifierRouter } from "../actions/contract-deployment/RiscZeroVerifierRouter/RiscZeroVerifierRouter.deploy";
 import { verifyContracts } from "../actions/verify/verifyContract";
 import hre from "hardhat";
 
 async function main() {
-  const [deployer] = await ethers.getSigners();
+  const [deployer] = await hre.ethers.getSigners();
   const routerDeployment = await deployRiscZeroVerifierRouter(deployer);
   const routerAddress = await routerDeployment.contract.getAddress();
   console.log(`Verifier router: ${routerAddress}`);
